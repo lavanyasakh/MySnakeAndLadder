54cf5b8a326a1b0037b4074a72e0ec2e44ad80e4
Lavanya <lavanyace929@gmail.com>
2021-02-10 20:46:31 +0530

    Added:A Dice Is Rolled For a Number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,11 @@
 public class SnakeAndLadder {
 	public static void main(String[] args)
 	{
-		int playerPosition = 0;
+		int player1Position = 0;
 		System.out.println("Please Start The Game");
+		int diceRoll = (int)Math.floor(Math.random() *10) %6+1;
+		player1Position += diceRoll;
+		System.out.println("Player1 Position =" +player1Position);
+		
 	}
 }
